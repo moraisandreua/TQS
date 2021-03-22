c95d341455ee8bbbecd7ef6c2e1d2d54677533d6
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2021-03-22 11:35:30 +0000

    lab1

---
+++ lab1/P1Stack/src/main/java/com/example/TqsStack.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class TqsStack<T>{
+    private LinkedList ll;
+    private Integer limit;
+
+    public TqsStack(){
+        this.ll=new LinkedList<>();
+        limit=-1;
+    }
+
+    public TqsStack(Integer limit){
+        this.ll=new LinkedList<>();
+        this.limit=limit;
+    }
+
+    public boolean isEmpty(){
+        return this.ll.isEmpty();
+    }
+
+    public void push(T element){
+        if(limit!=-1 && ll.size() < limit){
+            this.ll.push(element); 
+        }else if(limit!=-1 && ll.size() >= limit){
+            throw new IllegalStateException(); 
+        }else{
+            this.ll.push(element);
+        }
+    }
+
+    public Object pop(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }else{
+            return ll.pop();
+        }
+    }
+
+    public Object peek(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }else{
+            return ll.peek();
+        }
+    }
+
+    public int size(){
+        return this.ll.size();
+    }
+}
---
+++ lab1/P2Euromillions/README.md
@@ -0,0 +1,4 @@
+# JaCoCo
+Code coverage is a software metric used to measure how many lines of our code are executed during automated tests.
+
+In this article we're going to stroll through some practical aspects of using JaCoCo – a code coverage reports generator for Java projects.
